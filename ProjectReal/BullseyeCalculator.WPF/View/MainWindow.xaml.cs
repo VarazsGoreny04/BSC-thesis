3b155bf2e69613824cf1961e8032ebed2e89af07
Viktor Ádám Trefiman <varazsgoreny765@gmail.com>
2026-04-21 05:03:50 +0200

    Adding a roughly working font resizer.

--- ProjectReal/BullseyeCalculator.WPF/View/MainWindow.xaml.cs
+++ ProjectReal/BullseyeCalculator.WPF/View/MainWindow.xaml.cs
@@ -16,9 +16,27 @@ public partial class MainWindow : Window
 
 		int titlebarColor = 0x232323;
 		_ = DwmSetWindowAttribute(new System.Windows.Interop.WindowInteropHelper(this).EnsureHandle(), 35, ref titlebarColor, Marshal.SizeOf(titlebarColor));
+
+		SizeChanged += new SizeChangedEventHandler((_, _) => WindowLoaded(Width, Height));
+		StateChanged += new EventHandler(
+			(_, _) => 
+			{ 
+				if (WindowState == WindowState.Maximized)
+					WindowLoaded(1000, 1000); 
+				else
+					WindowLoaded(Width, Height); 
+			}
+		);
 	}
 
 
 	[LibraryImport("dwmapi.dll")]
 	private static partial int DwmSetWindowAttribute(IntPtr windowHandle, int attributeID, ref int attributeValue, int attributeSize);
+
+	private void WindowLoaded(double width, double height)
+	{
+		double controlSize = Math.Clamp(Math.Min(width, height) / 20, 14, 28);
+		Application.Current.Resources.Remove("ControlFontSize");
+		Application.Current.Resources.Add("ControlFontSize", controlSize);
+	}
 }
